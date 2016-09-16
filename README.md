f03c42a51ef7f508f13a8500d06e64e073d3c4b1
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-09-16 16:10:19 -0300

    initial commit. reveal.js-3.3.0

---
+++ README.md
@@ -0,0 +1,52 @@
+[![Creative Commons License](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-sa/4.0/)
+
+# Crystal Presentations
+
+This is a crystal playground that uses [Reveal.js](//lab.hakim.se/reveal-js) to build awesome presentations capable of running crystal code.
+
+The slides are splitted across multiple files.
+
+## Writing slides
+
+Fork/clone and write your own slides in `./slides`.
+Use the normal git branching so you can update the slide engine in the future.
+This will also promote sharing the slides (if you wish).
+
+Create `.html` and `.md` files for each slide in `./slides` folder.
+Slides will appear in alphabetical order.
+
+Numbering them as:
+
+1. `010-`
+2. `020-`
+
+will allow you to easy put slides in between ;-).
+
+In a `.md` file you can use ````playground` to include a interactive playground snippet.
+
+## Compile slides
+
+```
+$ ./bin/build-slides
+```
+
+```
+$ crystal play
+```
+
+Open [http://localhost:8080/workbook/playground/slides](http://localhost:8080/workbook/playground/slides).
+
+## Live reload
+
+Start a crystal playground in a terminal and run `./bin/watch`. It will look for changes in `./slides` compile them and reload the current chrome tab.
+
+```
+$ crystal play
+$ ./bin/watch
+```
+
+### ./bin/watch Requirements
+
+* [entr](http://entrproject.org/)
+* [chrome-cli](https://github.com/prasmussen/chrome-cli)
+
---
+++ src/build.cr
@@ -0,0 +1,64 @@
+require "ecr"
+
+abstract class Slide
+  def build
+  end
+end
+
+class MarkupSlide < Slide
+  def initialize(@content : String)
+  end
+
+  ECR.def_to_s "#{__DIR__}/_markup.html.ecr"
+end
+
+class MarkdownSlide < Slide
+  def initialize(@file : String, @output_dir : String)
+    @content = File.read(@file)
+  end
+
+  def build
+    @content = @content.sub /^```playground(.*)^```/m do |str, match|
+      base_slide_name = File.basename(@file, File.extname(@file))
+      code_file_name = "snippet-#{base_slide_name}"
+      File.write("#{@output_dir}/#{code_file_name}.cr", match[1].lstrip)
+      "<iframe src='#{code_file_name}' class='stretch'></iframe>"
+    end
+  end
+
+  ECR.def_to_s "#{__DIR__}/_markdown.html.ecr"
+end
+
+class Presentation
+  getter slides = [] of Slide
+
+  ECR.def_to_s "#{__DIR__}/slides.html.ecr"
+end
+
+slides_dir = "#{__DIR__}/../slides/"
+output_dir = "#{__DIR__}/../playground/"
+p = Presentation.new
+
+Dir["#{slides_dir}/*"].each do |file|
+  slide = case File.extname(file)
+          when ".html"
+            MarkupSlide.new(File.read(file))
+          when ".md"
+            MarkdownSlide.new(file, output_dir)
+          else
+            puts "ignoring: #{file}" unless file.ends_with?(".gitkeep")
+            nil
+          end
+  if slide
+    slide.build
+    p.slides << slide
+  end
+end
+
+print "Slides build: "
+
+File.open("#{output_dir}/slides.html", "w") do |file|
+  p.to_s(file)
+end
+
+puts "ok (#{Time.now})"
