8facc2724f6002baf7a8a980fdebb5de794ac8f0
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-09-17 01:11:57 -0300

    add frontmatter support on slides to determine transitions

--- src/build.cr
+++ src/build.cr
@@ -1,24 +1,37 @@
 require "ecr"
 require "file_utils"
+require "yaml"
 
 abstract class Slide
+  def initialize(@file : String, @output_dir : String)
+    @content = File.read(@file)
+    @frontmatter = YAML::Any.new(nil)
+  end
+
   def build
+    if @content.starts_with?("---") && (m = @content.match(/^---\s*$/m, 1))
+      @frontmatter = YAML.parse(m.pre_match)
+      @content = m.post_match.lstrip
+    end
+  end
+
+  def data
+    String::Builder.build do |io|
+      if (val = (@frontmatter.raw.as?(Hash).try &.["transition"]?))
+        io << " data-transition=\"#{val}\" "
+      end
+    end
   end
 end
 
 class MarkupSlide < Slide
-  def initialize(@content : String)
-  end
-
   ECR.def_to_s "#{__DIR__}/_markup.html.ecr"
 end
 
 class MarkdownSlide < Slide
-  def initialize(@file : String, @output_dir : String)
-    @content = File.read(@file)
-  end
-
   def build
+    super
+
     @content = @content.sub /^```playground(.*)^```/m do |str, match|
       base_slide_name = File.basename(@file, File.extname(@file))
       code_file_name = "snippet-#{base_slide_name}"
@@ -48,7 +61,7 @@ p = Presentation.new
 Dir["#{slides_dir}/*"].each do |file|
   slide = case File.extname(file)
           when ".html"
-            MarkupSlide.new(File.read(file))
+            MarkupSlide.new(file, output_dir)
           when ".md"
             MarkdownSlide.new(file, output_dir)
           else
